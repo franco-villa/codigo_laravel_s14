14a79218a60bd69d5e14143f37d8568e4536e852
franco-villa <t512700120@unitru.edu.pe>
2024-08-11 12:42:36 -0500

    Base de datos actualizada

--- bd/laravel.sql
+++ bd/laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-08-2024 a las 08:51:58
+-- Tiempo de generación: 11-08-2024 a las 19:41:27
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `url`, `created_at`, `updated_at`) VALUES
+(1, 'Categoria 1', 'categoria-1', '2024-08-10 14:47:34', '2024-08-10 14:47:34'),
+(3, 'Categoria 2', 'categoria-2', '2024-08-10 15:39:23', '2024-08-10 15:39:23'),
+(4, 'Categoria 3', 'categoria-3', '2024-08-10 15:43:23', '2024-08-10 15:43:23'),
+(5, 'Categoria 4', 'categoria-4', '2024-08-10 15:43:23', '2024-08-10 15:43:23'),
+(6, 'Categoria 5', 'categoria-5', '2024-08-10 15:44:04', '2024-08-10 15:44:04');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `jobs`
 --
 
@@ -36,6 +61,14 @@ CREATE TABLE `jobs` (
   `available_at` int(10) UNSIGNED NOT NULL,
   `created_at` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `queue`, `payload`, `attempts`, `reserved_at`, `available_at`, `created_at`) VALUES
+(2, 'default', '{\"uuid\":\"224f0979-7afc-4fe2-a77d-25a11aedf791\",\"displayName\":\"App\\\\Listeners\\\\OptimizeServicioImage\",\"job\":\"Illuminate\\\\Queue\\\\CallQueuedHandler@call\",\"maxTries\":null,\"maxExceptions\":null,\"failOnTimeout\":false,\"backoff\":null,\"timeout\":null,\"retryUntil\":null,\"data\":{\"commandName\":\"Illuminate\\\\Events\\\\CallQueuedListener\",\"command\":\"O:36:\\\"Illuminate\\\\Events\\\\CallQueuedListener\\\":20:{s:5:\\\"class\\\";s:35:\\\"App\\\\Listeners\\\\OptimizeServicioImage\\\";s:6:\\\"method\\\";s:6:\\\"handle\\\";s:4:\\\"data\\\";a:1:{i:0;O:24:\\\"App\\\\Events\\\\ServicioSaved\\\":1:{s:8:\\\"servicio\\\";O:45:\\\"Illuminate\\\\Contracts\\\\Database\\\\ModelIdentifier\\\":5:{s:5:\\\"class\\\";s:19:\\\"App\\\\Models\\\\Servicio\\\";s:2:\\\"id\\\";i:2;s:9:\\\"relations\\\";a:0:{}s:10:\\\"connection\\\";s:5:\\\"mysql\\\";s:15:\\\"collectionClass\\\";N;}}}s:5:\\\"tries\\\";N;s:13:\\\"maxExceptions\\\";N;s:7:\\\"backoff\\\";N;s:10:\\\"retryUntil\\\";N;s:7:\\\"timeout\\\";N;s:13:\\\"failOnTimeout\\\";b:0;s:17:\\\"shouldBeEncrypted\\\";b:0;s:3:\\\"job\\\";N;s:10:\\\"connection\\\";N;s:5:\\\"queue\\\";N;s:5:\\\"delay\\\";N;s:11:\\\"afterCommit\\\";N;s:10:\\\"middleware\\\";a:0:{}s:7:\\\"chained\\\";a:0:{}s:15:\\\"chainConnection\\\";N;s:10:\\\"chainQueue\\\";N;s:19:\\\"chainCatchCallbacks\\\";N;}\"}}', 0, NULL, 1723303697, 1723303697),
+(3, 'default', '{\"uuid\":\"cb7260ec-8cbe-4f32-88e3-7c67998ace02\",\"displayName\":\"App\\\\Listeners\\\\OptimizeServicioImage\",\"job\":\"Illuminate\\\\Queue\\\\CallQueuedHandler@call\",\"maxTries\":null,\"maxExceptions\":null,\"failOnTimeout\":false,\"backoff\":null,\"timeout\":null,\"retryUntil\":null,\"data\":{\"commandName\":\"Illuminate\\\\Events\\\\CallQueuedListener\",\"command\":\"O:36:\\\"Illuminate\\\\Events\\\\CallQueuedListener\\\":20:{s:5:\\\"class\\\";s:35:\\\"App\\\\Listeners\\\\OptimizeServicioImage\\\";s:6:\\\"method\\\";s:6:\\\"handle\\\";s:4:\\\"data\\\";a:1:{i:0;O:24:\\\"App\\\\Events\\\\ServicioSaved\\\":1:{s:8:\\\"servicio\\\";O:45:\\\"Illuminate\\\\Contracts\\\\Database\\\\ModelIdentifier\\\":5:{s:5:\\\"class\\\";s:19:\\\"App\\\\Models\\\\Servicio\\\";s:2:\\\"id\\\";i:5;s:9:\\\"relations\\\";a:0:{}s:10:\\\"connection\\\";s:5:\\\"mysql\\\";s:15:\\\"collectionClass\\\";N;}}}s:5:\\\"tries\\\";N;s:13:\\\"maxExceptions\\\";N;s:7:\\\"backoff\\\";N;s:10:\\\"retryUntil\\\";N;s:7:\\\"timeout\\\";N;s:13:\\\"failOnTimeout\\\";b:0;s:17:\\\"shouldBeEncrypted\\\";b:0;s:3:\\\"job\\\";N;s:10:\\\"connection\\\";N;s:5:\\\"queue\\\";N;s:5:\\\"delay\\\";N;s:11:\\\"afterCommit\\\";N;s:10:\\\"middleware\\\";a:0:{}s:7:\\\"chained\\\";a:0:{}s:15:\\\"chainConnection\\\";N;s:10:\\\"chainQueue\\\";N;s:19:\\\"chainCatchCallbacks\\\";N;}\"}}', 0, NULL, 1723396920, 1723396920);
 
 -- --------------------------------------------------------
 
@@ -58,7 +91,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 2),
 (3, '2024_06_30_142529_create_servicios_table', 3),
 (4, '2024_07_27_052617_add_image_field_to_servicios_table', 3),
-(5, '2024_08_04_064711_create_jobs_table', 4);
+(5, '2024_08_04_064711_create_jobs_table', 4),
+(8, '2024_08_11_035232_create_categories_table', 5);
 
 -- --------------------------------------------------------
 
@@ -80,6 +114,7 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `servicios` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `titulo` varchar(255) NOT NULL,
   `descripcion` text NOT NULL,
@@ -91,13 +126,13 @@ CREATE TABLE `servicios` (
 -- Volcado de datos para la tabla `servicios`
 --
 
-INSERT INTO `servicios` (`id`, `image`, `titulo`, `descripcion`, `created_at`, `updated_at`) VALUES
-(1, 'images/Rf5Ejan5l3aFnkFhReITOPGR7ry8oBN7Pu7RIN7j.jpg', 'Mantenimiento', 'Descripción del servicio de Mantenimiento', '2024-06-08 15:05:42', '2024-07-29 22:57:59'),
-(2, 'images/qUjjfHCOZ7r73cGIk2DVFhRvm1797GwTJUaIJEWS.jpg', 'Afinamiento', 'Descripción del servicio de Afinamiento', '2024-06-08 15:05:42', '2024-08-04 11:49:58'),
-(3, 'images/bLbZq7F6NRVL2dNZG5Z1eSqzXv4VyQMH7baHgSyw.jpg', 'Cambio de Aceite', 'Descripción del servicio de Cambio de Aceite', '2024-06-07 15:07:15', '2024-07-29 23:02:34'),
-(4, 'images/uI5dvR4eg7xV4kRNY5C0WI4qQqJBpT84rxyC7GX5.jpg', 'Lavado tipo salón', 'Descripción del servicio de Lavado tipo salón', '2024-06-07 15:07:15', '2024-07-29 23:07:50'),
-(5, NULL, 'Servicio 5', 'Dscp. 5', '2024-06-30 14:09:00', '2024-06-30 17:54:23'),
-(6, NULL, 'Servicio 6', 'Dscrp. 6', '2024-06-30 17:54:51', '2024-06-30 17:54:51');
+INSERT INTO `servicios` (`id`, `category_id`, `image`, `titulo`, `descripcion`, `created_at`, `updated_at`) VALUES
+(1, 4, 'images/Rf5Ejan5l3aFnkFhReITOPGR7ry8oBN7Pu7RIN7j.jpg', 'Mantenimiento', 'Descripción del servicio de Mantenimiento', '2024-06-08 15:05:42', '2024-08-11 22:20:07'),
+(2, 1, 'images/5qcA1GLuxIRCdr3qm7aWnZOpRPMVJtC8WDiYibHA.jpg', 'Afinamiento', 'Descripción del servicio de Afinamiento', '2024-06-08 15:05:42', '2024-08-11 20:42:22'),
+(3, 3, 'images/bLbZq7F6NRVL2dNZG5Z1eSqzXv4VyQMH7baHgSyw.jpg', 'Cambio de Aceite', 'Descripción del servicio de Cambio de Aceite', '2024-06-07 15:07:15', '2024-08-11 22:19:50'),
+(4, 6, 'images/uI5dvR4eg7xV4kRNY5C0WI4qQqJBpT84rxyC7GX5.jpg', 'Lavado tipo salón', 'Descripción del servicio de Lavado tipo salón', '2024-06-07 15:07:15', '2024-08-11 22:40:05'),
+(5, 3, 'images/HEz0t0txfzieT3ylJdkvp9atJw1kglQONZmj69iS.jpg', 'Servicio 5', 'Dscp. 5', '2024-06-30 14:09:00', '2024-08-11 22:39:49'),
+(6, 5, NULL, 'Servicio 6', 'Dscrp. 6', '2024-06-30 17:54:51', '2024-08-11 22:22:19');
 
 -- --------------------------------------------------------
 
@@ -127,6 +162,13 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_a
 --
 
 --
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_url_unique` (`url`);
+
+--
 -- Indices de la tabla `jobs`
 --
 ALTER TABLE `jobs`
@@ -149,7 +191,8 @@ ALTER TABLE `password_resets`
 -- Indices de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `servicios_category_id_foreign` (`category_id`);
 
 --
 -- Indices de la tabla `users`
@@ -162,16 +205,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
@@ -184,6 +233,16 @@ ALTER TABLE `servicios`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `servicios_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
